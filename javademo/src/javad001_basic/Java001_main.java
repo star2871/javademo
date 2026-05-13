97cfa04be97bdcfa963581dd91038ded72e7b6a8
star2871 <star2871@example.com>
2026-05-13 10:30:47 +0900

    ADD 20260513 First

---
+++ javademo/src/javad001_basic/Java001_main.java
@@ -0,0 +1,10 @@
+package javad001_basic;
+
+public class Java001_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+	}
+
+}
